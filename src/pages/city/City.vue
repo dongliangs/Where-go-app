93691c78556b2f1042bfd43d279d6788cc358c0b
dongliangs <15993807059@163.com>
2019-07-17 23:27:04 +0800

    编写city字母滑动

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,8 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
-        <city-list :cities="cities" :hot="hotCities"></city-list>
-        <city-alphabet :cities="cities"></city-alphabet>
+        <city-list :cities="cities" :hot="hotCities" :letter="letter"></city-list>
+        <city-alphabet :cities="cities" @change="handleLetterChange"></city-alphabet>
     </div>
 </template>
 <script>
@@ -22,7 +22,8 @@ export default {
     data () {
         return {
             cities: {},
-            hotCities: []
+            hotCities: [],
+            letter:''
         }
     },
     methods:{
@@ -37,6 +38,9 @@ export default {
                 this.cities = res.data.cities;
                 this.hotCities = res.data.hotCities;
             }
+        },
+        handleLetterChange (letter){
+            this.letter = letter
         }
     },
     mounted () {
--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,6 +1,11 @@
 <template>
     <ul class="alphabet">
-        <li class="alphabet-item" v-for="(item, key) of cities" :key="key">{{key}}</li>
+        <li class="alphabet-item" v-for="item of letters" :key="item"
+        :ref="item"
+        @touchstart="handleTouchStart"
+        @touchmove="handleTouchMove"
+        @touchend="handleTouchEnd" 
+        @click="handleLetterClick">{{item}}</li>
     </ul>
 </template>
 
@@ -9,6 +14,45 @@ export default {
     name: 'CityAlphabet',
     props:{
         cities: Object
+    },
+    data () {
+        return {
+            touchStatus: false //定义标识位
+        }
+    },
+    computed: {
+        letters(){
+            const letters = [];
+            for (let i in this.cities) {
+                letters.push(i)
+            }
+            return letters;
+        }
+    },
+    methods: {
+        handleLetterClick(e){
+            //console.log(e.target.innerText)
+            this.$emit('change', e.target.innerText)
+        },
+        handleTouchStart () {
+            this.touchStatus = true;
+        },
+        handleTouchMove (e) {
+            if (this.touchStatus) {
+                const startY = this.$refs['A'][0].offsetTop //获取首字母距离滚动顶部的大小
+                const touchY = e.touches[0].clientY - 79
+                //console.log(touchY)
+                const index = Math.floor((touchY - startY) / 25)
+                if (index >= 0 && index < this.letters.length){
+                    this.$emit('change', this.letters[index])
+                }
+               
+            }
+        },
+        handleTouchEnd () {
+            this.touchStatus = false
+        }
+
     }
 }
 </script>
@@ -26,7 +70,8 @@ export default {
         width:.4rem;
         .alphabet-item{
             text-align: center;
-            line-height: .44rem;
+            font-size: .4rem;
+            line-height: .5rem;
             color:$bgColor;
         }
     }
